135d61c3079d859f29e364c8e0a45329c509d8f2
Moldarakhimov <t.molarakhimov@gmail.com>
2023-11-18 21:06:02 +0600

    Добавление метки файл

--- CsvAnalyst.py
+++ CsvAnalyst.py
@@ -7,4 +7,12 @@ window=tk.Tk()
 window.geometry("550x550")
 window.title("Программа анализа .csv файлов")
 
+# Создание меток вывода
+label_00 = tk.Label(text = "Файл:")
+label_00.grid(row=0, column=0, padx=10, pady=10, sticky="e")
+
+label_01 = tk.Label(text = "")
+label_01.grid(row=0, column=1, sticky="w")
+
+
 window.mainloop()
